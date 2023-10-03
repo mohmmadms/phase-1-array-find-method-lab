acc211a80713507b9565090c2187a5bedc625ec3
mohmmad Smadi <mohmmadmms20@gmail.com>
2023-10-04 01:57:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2015", result: "L"},
+    {year: "2016", result: "N/A"},
+    {year: "2015", result: "W"}
+    //...
+  ]
+  function superbowlWin(myArray){
+    const winningYearObj = myArray.find(superbowl => superbowl.result === "W");
+    
+    if (winningYearObj){
+    return winningYearObj.year}
+    else{
+        return undefined;
+    }
+
+  }
+  
